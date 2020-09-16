33fe7b7de2d9ee4295f19c0845ea0cafc847512d
NAleshina <minerva987@gmail.com>
2020-09-16 21:07:12 +0300

    Initial commit

---
+++ src/test/java/ru/netology/AppCardDeliveryTest.java
@@ -0,0 +1,83 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public class AppCardDeliveryTest {
+
+    @Test
+    void shouldCardDeliveryWithAllData() {
+        LocalDate dateNow = LocalDate.now();
+        dateNow = dateNow.plusDays(10);
+        DateTimeFormatter formatForDateNow = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        open("http://localhost:9999");
+        $("[placeholder='Город']").setValue("Нарьян-Мар");
+        JavascriptExecutor js = (JavascriptExecutor)getWebDriver();
+        js.executeScript("document.querySelector('.calendar-input__custom-control input.input__control').value = '" + formatForDateNow.format(dateNow) + "'");
+        $("[name='name']").setValue("Кавендиш Генри");
+        $("[name='phone']").setValue("+71112223344");
+        $("[class=checkbox__box]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(withText("Успешно!")).waitUntil(visible, 15000);
+    }
+
+    @Test
+    void shouldCardDeliveryWithoutCity(){
+        open("http://localhost:9999");
+        $("[name='name']").setValue("Кавендиш Генри");
+        $("[name='phone']").setValue("+71112223344");
+        $("[class=checkbox__box]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=city] span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldCardDeliveryWithoutName(){
+        open("http://localhost:9999");
+        $("[placeholder='Город']").setValue("Нарьян-Мар");
+        $("[name='phone']").setValue("+71112223344");
+        $("[class=checkbox__box]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=name] span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldCardDeliveryWithEngName(){
+        open("http://localhost:9999");
+        $("[placeholder='Город']").setValue("Нарьян-Мар");
+        $("[name='name']").setValue("Cavendish Henry");
+        $("[name='phone']").setValue("+71112223344");
+        $("[class=checkbox__box]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=name] span.input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldCardDeliveryWithoutPhone(){
+        open("http://localhost:9999");
+        $("[placeholder='Город']").setValue("Нарьян-Мар");
+        $("[name='name']").setValue("Кавендиш Генри");
+        $("[class=checkbox__box]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=phone] span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldCardDeliveryWithoutCheckbox(){
+        open("http://localhost:9999");
+        $("[placeholder='Город']").setValue("Нарьян-Мар");
+        $("[name='name']").setValue("Кавендиш Генри");
+        $("[name='phone']").setValue("+71112223344");
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=agreement]").shouldHave(Condition.cssClass("input_invalid"));
+    }
+}
